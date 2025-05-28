f5e6125af565c67eba9bf05e181b9b0a4f2477bf
Maga-nan <160400346+Maga-nan@users.noreply.github.com>
2025-05-28 16:19:55 +0300

    !

--- lab7/lab7/src/main/java/com/example/lab/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lab.Repository;
-
-import com.example.lab.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
