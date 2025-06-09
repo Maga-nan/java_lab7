56280069173b193ea6a5f5d09e30fe9a573a54a3
Maga-nan <160400346+Maga-nan@users.noreply.github.com>
2025-06-09 16:55:19 +0300

    Delete lab7/target/classes directory

--- lab7/target/classes/static/script.js
+++
@@ -1,5 +0,0 @@
-function convert() {
-    const timestamp = document.getElementById('timestamp').value;
-    const date = new Date(timestamp * 1000);
-    document.getElementById('result').textContent = date.toLocaleString();
-}
